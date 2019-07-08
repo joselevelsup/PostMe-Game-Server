3fdd20340eb0fad225b7c3a2425db93e182a3641
joselevelsup <hipnotickid12@gmail.com>
2019-07-07 23:51:00 -0400

    adding tests

--- src/tests/room.ts
+++ src/tests/room.ts
@@ -9,7 +9,7 @@ chai.use(chaiHttp);
 let expect: any = chai.expect;
 
 describe("Rooms", (): void => {
-	it("should create a room", (done: any): void => {
+	it("should create a public room", (done: any): void => {
 		chai.request(server).post("/room").send({
 			roomNumber: "0a1b2c",
 			roomType: RoomType.Public
@@ -23,7 +23,21 @@ describe("Rooms", (): void => {
 		});
 	});
 
-	it("should get all public rooms", (done: any) => {
+	it("should create a private room", (done: any): void => {
+		chai.request(server).post("/room").send({
+			roomNumber: "1ab2c3",
+			roomType: RoomType.Private
+		}).end((err: any, res: any) => {
+			if(err){
+				done(err);
+			} else {
+				expect(res).to.have.status(200);
+				done();
+			}
+		})
+	});
+
+	it("should get all public rooms", (done: any): void => {
 		chai.request(server).get("/room/public").end((err: any, res: any) => {
 			if(err){
 				done(err);
